7da5dca1311df9d8ba08735d29017d82535c73ae
jumang4423 <catontheskate4423@gmail.com>
2022-01-19 01:30:19 +0900

    result patturn

--- pages/index.js
+++ pages/index.js
@@ -1,30 +1,51 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import axios from 'axios'
+
+// result patturn
+class Fetched {
+  ok
+  err 
+}
 
 // data fetching async func
 const getData = async () => {
   try {
     // TODO: put URL ( which is get )
-    const { data } = await axios.get("https://...SOME_URL")
-    return data
+    const { data } = await axios.get("https://api.jsonin.io/b/6110a49bd5667e403a3c118f/129")
+
+    // ok result
+    let ok_fetched = new Fetched
+    ok_fetched.ok = data
+    return ok_fetched
 
   } catch (e) {
-    alert(e)
-    return null
+    // err result
+    let err_fetched = new Fetched
+    err_fetched.err = e
+    return err_fetched
   }
 }
 
 export default function Home() {
+
+  const [data, setData] = useState({})
+
   useEffect(async () => {
     const fetched = await getData()
-    if (fetched != null) {
-      alert(fetched)
+    // patturn match
+    if (fetched.ok != null) {
+      setData(fetched.ok)
+    } else {
+      setData(fetched.err)
     }
   }, [])
 
   return (
     <div>
-      dom rendered
+      data: 
+      {
+        JSON.stringify(data)
+      }
     </div>
   )
 }
